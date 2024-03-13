04ba976163b2f52ebc67c7fa4133da2231816ae7
Roucool_PvP <lucastag77@gmail.com>
2024-03-13 17:02:25 +0100

    [NW] Bootstrap clean

--- Assets/Scriptss/Entrypoint.cs
+++ Assets/Scriptss/Entrypoint.cs
@@ -1,18 +1,13 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
+public class GameApp
 {
-    // Start is called before the first frame update
-    void Start()
+    [RuntimeInitializeOnLoadMethod]
+    static void Bootstrap()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var MyGameManager = new GameObject("Game");
+        MyGameManager.AddComponent<GameManager>();
     }
 }
