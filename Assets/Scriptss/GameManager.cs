e1d1210b2a3341f270933ff4defffa86e5bb3a10
Roucool_PvP <lucastag77@gmail.com>
2024-03-21 14:04:37 +0100

    DontDestroy

--- Assets/Scriptss/GameManager.cs
+++ Assets/Scriptss/GameManager.cs
@@ -4,59 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField, Range(2, 500)]
-    private int AmountOfRooms = 5;
-
     public static GameManager Instance;
-    private int Room;
-    private List<GameObject> Rooms;
-    private GameObject CurrentRoom;
     // Start is called before the first frame update
     void Start()
     {
-        if (Instance == null)
+        if (Instance == null) {
             Instance = this;
-        else {
+            DontDestroyOnLoad(gameObject);
+        } else {
             Destroy(gameObject);
             return;
         }
-
-        GenerateRooms();
-        SetRoom(0, PlayerSpawner.CardinalDirection.NORTH);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void SetRoom(int room, PlayerSpawner.CardinalDirection direction)
-    {
-        Room = room;
-        GameObject RoomObject = this.Rooms[Room];
-        RoomObject.SetActive(true);
-
-        if (CurrentRoom != null)
-            CurrentRoom.SetActive(false);
-
-        CurrentRoom = RoomObject;
-
-        GameObject Player = PlayerFactory.GetOrCreatePlayer();
-    }
-
-    public void GenerateRooms()
-    {
-        // Add here code to generate rooms
-
-        this.Rooms = new List<GameObject>();
-
-        for (int i = 0; i < AmountOfRooms; i++)
-        {
-            string RoomName = "Room " + i;
-            GameObject RoomObject = GameObject.Find(RoomName);
-            RoomObject.SetActive(false);
-            this.Rooms.Add(RoomObject);
-        }
+        // Calcul position caméra selon position joueur
     }
 }
